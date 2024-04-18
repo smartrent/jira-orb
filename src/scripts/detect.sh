3fb68c468b80cebb7cec45addbb733a2afe07af7
Jon Carstens <jon.carstens@smartrent.com>
2024-04-18 13:15:20 -0600

    Support detecting state with custom script
    This allows a user to use their own logic to determine the state that should
    be applied to tickets. It is useful in conditions where every run might be
    slightly different such as a rollback or conditions not met as part of the
    flow but another status besides "failed" is desirable

--- src/scripts/detect.sh
+++ src/scripts/detect.sh
@@ -2,6 +2,14 @@
 
 jira_status=${JIRA_VAL_STATE:-$JOB_STATUS}
 
+if [[ "$JOB_STATUS" != "failed" && -n "$JIRA_VAL_STATE_SCRIPT" ]]; then
+  JIRA_VAL_STATE=$(/bin/bash -c "$JIRA_VAL_STATE_SCRIPT" | tr -d ' ' | tr -d '\n')
+  if [[ ! "$JIRA_VAL_STATE" =~ ^(unknown|pending|in_progress|cancelled|failed|rolled_back|successful)$ ]]; then
+    echo "Invalid Jira status detected: $JIRA_VAL_STATE"
+    exit 1
+  fi
+fi
+
 if [[ "${JIRA_VAL_JOB_TYPE}" == "build" && "${JIRA_VAL_STATE}" == "rolled_back" ]]; then
   echo "Cannot use 'rolled_back' build job type. Using '${JOB_STATUS}'"
   jira_status="${JOB_STATUS}"
